320b3a5cff29b4bdf22b618cca88c03b7ee5fc08
Tomstyleuk <35295995+Tomstyleuk@users.noreply.github.com>
2020-04-01 10:46:29 +0200

    Add files via upload

--- CodeGrit JS(piano)/main.js
+++ CodeGrit JS(piano)/main.js
@@ -5,9 +5,10 @@ function keyPlaying(e) {//関数の機能を追加する
     audio.play(); //音を鳴らす
     audio.currentTime = 0; //音が再生される時間も初期化
     key.classList.add('playing'); //.playingと言うclass属性を追加
+    console.log(key);      // <div class="key playing" data-key="65">
+    console.log(audio);    // <audio data-key="70" src="sounds/fa.wav">
 }
 window.addEventListener('keydown', keyPlaying); //イベントと紐付けをして関数を呼び出す式。
-console.log('keydown', keyPlaying);
 
 function keyPlayingReset() {
     const key1 = document.getElementsByClassName('key')//場所の指定。keyのclass属性にplayingが追加されたから排除するための場所指定
